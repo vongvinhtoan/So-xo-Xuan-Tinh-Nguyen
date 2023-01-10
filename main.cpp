43a82e4a2622979b803d4ea5bae31e5487e23f68
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-01-10 14:35:15 +0700

    Font and sprite

--- main.cpp
+++ main.cpp
@@ -1,18 +1,36 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+sf::CircleShape shape(100, 10000);
+
+void renderingThread(sf::RenderWindow* window)
+{
+    shape.setFillColor(sf::Color::Green);
+    
+    window->setActive(true);
+
+    while (window->isOpen())
+    {
+        window->draw(shape);
+
+        window->display();
+    }
+}
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Haha");
 
-    // run the program as long as the window is open
+    window.setActive(false);
+
+    sf::Thread thread(&renderingThread, &window);
+    thread.launch();
+
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed);
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
     }
