86df002ee4958e4017b156b50e6a1d5c65eca439
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-01-10 13:31:41 +0700

    idk

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
-#include <SFML/Graphics.hpp>
-
-using namespace sf;
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-    sf::CircleShape shape(250.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Window window(sf::VideoMode(800, 600), "My window");
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed);
                 window.close();
         }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
 
     return 0;
