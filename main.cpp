4113a2db5e70d33c4c51f1d6904f05807498b410
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-01-11 13:18:43 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <SFML/Graphics.hpp>
 #include <string.h>
 #include <iostream>
+#include <windows.h>
+#include <set>
+#include <fstream>
 #include "random.h"
 
 int lock = 0;
 std::string state, statelock;
+std::set<int> appeared;
+
+std::string num2string(int a)
+{
+    std::string s;
+    for(int i=0; i<3; i++, a/=10) s += a%10 + '0';
+    std::reverse(s.begin(), s.end());
+    return s;
+}
 
 void gen(Random &rng)
 {
@@ -43,7 +55,7 @@ void renderingThread(sf::RenderWindow* window)
         text.setFillColor(sf::Color(190, 38, 51));
         text.setPosition(sf::Vector2f(854 + 143 * i, 384));
     }
-    Random rng(11235813);
+    Random rng(22112004);
 
     window->setFramerateLimit(30);
 
@@ -60,14 +72,31 @@ void renderingThread(sf::RenderWindow* window)
     }
 }
 
+void loadData()
+{
+    std::ifstream fi;
+    fi.open("winner.txt");
+    int x;
+    while(fi>>x)
+        appeared.insert(x);
+    fi.close();
+}
+
 int main()
 {
+    HWND hWnd = GetForegroundWindow();
+    ShowWindow( hWnd, SW_HIDE );
+
+    loadData();
+
     sf::RenderWindow window(sf::VideoMode(1453, 756), "Haha");
 
     window.setActive(false);
 
     sf::Thread thread(&renderingThread, &window);
     thread.launch();
+
+    Random rng(22112004);
 
     while (window.isOpen())
     {
@@ -78,17 +107,17 @@ int main()
             {
                 window.close();
             }
-            
+
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
             {
                 int id = 0;
                 if(lock == 1) id = 1;
                 else if(lock == 3) id = 2;
 
+                if(!lock) statelock = num2string(rng.rand(1, 640));
+
                 lock = lock << 1 | 1;
                 lock &= (1<<3) - 1;
-
-                statelock[id] = state[id];
             }
 
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R)
